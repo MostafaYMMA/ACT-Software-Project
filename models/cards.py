c530d6b5eae8fb20c1039f8128b4c6791cfadf64
Orashy23 <omarorashy8@gmail.com>
2026-07-07 14:19:10 +0300

    Models

---
+++ models/cards.py
@@ -0,0 +1,5 @@
+class Record: 
+    def __init__(self, EmployeeName, ProjectNumber, TaskNumber,):
+        self.EmployeeName = EmployeeName
+        self.ProjectNumber = ProjectNumber
+        self.TaskNumber = TaskNumber
